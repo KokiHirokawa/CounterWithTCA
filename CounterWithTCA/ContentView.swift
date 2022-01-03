1063def4d08b3e3dd323fa117195e31cb65e3d8c
KokiHirokawa <kitakitsune1110@gmail.com>
2022-01-03 14:44:46 +0900

    Initial Commit

---
+++ CounterWithTCA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CounterWithTCA
+//
+//  Created by 廣川昂紀 on 2022/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CounterWithTCA/CounterWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterWithTCAApp.swift
+//  CounterWithTCA
+//
+//  Created by 廣川昂紀 on 2022/01/03.
+//
+
+import SwiftUI
+
+@main
+struct CounterWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
